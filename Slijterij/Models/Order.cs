6b18ca032fd46836b28b6f33488d2a9fbd1f0ccb
Meintje Diepstraten <maankozijn@gmail.com>
2020-02-26 14:40:43 +0100

    Add Models

---
+++ Slijterij/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slijterij.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public int CustomerID { get; set; }
+        public int EmployeeID { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public Customer Customer { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
---
+++ Slijterij/Models/OrderProduct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Slijterij.Models
+{
+    public class OrderProduct
+    {
+        [Key, Column(Order = 0)]
+        public int OrderID { get; set; }
+        [Key, Column(Order = 1)]
+        public int ProductID { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
