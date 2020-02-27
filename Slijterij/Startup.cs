4287bff5cb29bab2aa4570a967436fb933ccb116
Meintje Diepstraten <maankozijn@gmail.com>
2020-02-27 14:27:32 +0100

    Add Xamarin.Forms project

--- Slijterij/Startup.cs
+++ Slijterij/Startup.cs
@@ -22,6 +22,8 @@ namespace Slijterij
         {
             services.AddDbContext<WhiskeyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+
+            services.Configure<IISServerOptions>(options => { options.AutomaticAuthentication = false; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
